04a92393582cde2b82c36816803fb6a13bc618e1
singhbhadauria946-svg <singhbhadauria946@gmail.com>
2025-08-11 15:35:44 +0530

    Initial

--- main.tf
+++ main.tf
@@ -1,3 +1,21 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+}
+
 resource "azurerm_resource_group" "rg" {
   name     = "my-tf-rg"
   location = "East US"
